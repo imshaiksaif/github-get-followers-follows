9abd78830cb0ed017d6c847578789e9ff60d9532
Saif Ali <imshaiksaif@gmail.com>
2021-01-21 14:57:18 +0530

    removed wraning in FE

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import Header from "./components/Header";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { UserForm } from "./components/UserForm";
 import ResultList from "./components/ResultList";
 
